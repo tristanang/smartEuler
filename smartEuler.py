509a03ad23177e5561828e6b4ed1309f1d72bcc0
Tristan Ang <tangtzeheng@wesleyan.edu>
2017-11-01 15:31:58 -0400

    added adaptive stepsize

--- smartEuler.py
+++ smartEuler.py
@@ -1,5 +1,23 @@
-def adaptiveEuler():
-    return
+def adaptiveEuler(f,tstart,tend,yinitial,h): #h instead of dt used because it is no longer a fixed quantity
+    tol = 0.005
+    saf = 0.9
+    
+    timelst = [tstart]
+    ylst = [yinitial]
+    
+    while timelst[-1] < tend:
+        df =  f(timelst[-1],ylst[-1])
+        y_one = ylst[-1] + (h * df)
+        y_half = ylst[-1] + (h/2)*(df)
+        y_twohalf = y_half + (h/2) * (f(timelst[-1]+(h/2),y_half))
+        error = abs(y_twohalf - y_one)
+        h = saf * h * min(max(tol/error,0.3),2)
+       
+        timelst.append(timelst[-1]+h)
+        y_next = ylst[-1] + h * df
+        ylst.append(y_next)
+    
+    return timelst,ylst
 
 def adaptiveRK45():
     return
--- test.py
+++ test.py
@@ -1,8 +1,9 @@
 from operator import sub #the subtraction function
 import math
 import mpmath
-import RKeuler
-import smartEuler
+from RKeuler import *
+from smartEuler import *
+import matplotlib.pyplot as plt
 
 def f_d(t,y):
     return math.exp(t) * math.sin(y)
@@ -12,10 +13,23 @@ def f(t): #f(0) = 1
         math.exp(1-math.exp(t))*mpmath.cot(0.5)
         )
     
-
 def calcSquareError(f,tlst,ylst):
     y_actual = list(map(f,tlst))
     errorlst = list(map(sub, y_actual, ylst)) 
     errorlst = list(map(lambda x: x**2, errorlst))
     return sum(errorlst)/len(tlst)
-    
+
+#Visual Tests
+
+def visualtest(df,f,method):
+    x,y = method(df,0,10,1,1)
+    y_act = list(map(f,x))
+    plt.plot(x,y,label='n_int')
+    plt.plot(x,y_act,label='act')
+    plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
+           ncol=2, mode="expand", borderaxespad=0.)
+    plt.show()
+
+#visualtest(f_d,f,adaptiveEuler)
+
+#Quantitative Tests
