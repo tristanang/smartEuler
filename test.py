6cd4e8b2620a872df25d6fce0aa1d7356e27e62e
Tristan Ang <tangtzeheng@wesleyan.edu>
2017-11-03 06:24:53 -0400

    fixing test

--- test.py
+++ test.py
@@ -26,15 +26,16 @@ def f2_d(t,y=0):
     return math.cos(t)
 
 def f3(t): #y(0) = 1
-    return math.exp(math.exp(t)-1)
+    return math.exp((t**2)/2)
 
 def f3_d(t,y):
-    return y*math.exp(t)
+    return y*t
 
 def f4(t): #y(0) = 1
-    return 1/(1-t)
+    return 1/(1+t**2)
+    
 def f4_d(t,y):
-    return y**2
+    return -2*t*y**2
 
 # List of Functions
 functions = [f,f1,f2,f3,f4]
@@ -66,7 +67,7 @@ def wrapper(df,initial,tstart,tend,dt,method=adaptiveEuler):
     return wrapped
 
 def timeWrapper(df,initial,tstart,tend,dt,method=adaptiveEuler):
-    #global wrapped
+
     wrapped = wrapper(df,initial,tstart,tend,dt,method=adaptiveEuler)
     
     return timeit(wrapped,number=10000)
@@ -81,10 +82,5 @@ def time3():
     return eulerMethod(f_d,0,5,1,0.05)
 
 
-
-#timeit('time1()',setup='from test import time1', number=10000)
-#timeit('time2()',setup='from test import time2', number=10000)
-
-#function to display twolists in image or ?... columns with the actual.
 
 #visualTest(f,f_d,1,0,10,1,adaptiveEuler)
